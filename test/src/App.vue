42d5bfe729500d05e1f634f321d4a548cb876aaa
363881055@qq.com <yfl@163.com>
2019-05-25 16:01:45 +0800

    1

--- test/src/App.vue
+++ test/src/App.vue
@@ -23,70 +23,70 @@
 
                     const map = new BMap.Map(document.createElement('div'));
 
-                    // navigator.geolocation.getCurrentPosition(position => {
-                    //     const ggPoint = new BMap.Point(position.coords.longitude, position.coords.latitude);
-                    //     const convertor = new BMap.Convertor();
-                    //     convertor.translate([ggPoint], 1, 5, data => {
-                    //         if (data.status === 0) {
-                    //             const bdPoint = data.points[0]
-                    //
-                    //             var myGeo = new BMap.Geocoder();
-                    //             myGeo.getLocation(bdPoint, function(result){
-                    //                 if (result){
-                    //                     alert(result.address);
-                    //                 }
-                    //             });
-                    //
-                    //             // const map = new BMap.Map(document.createElement('div'));
-                    //             //
-                    //             // map.centerAndZoom(bdPoint, 19);
-                    //             // const options = {
-                    //             //     onSearchComplete: function (results) {
-                    //             //         if (local.getStatus() === BMAP_STATUS_SUCCESS) {
-                    //             //             return resolve(results.getPoi(0).title)
-                    //             //         }
-                    //             //     },
-                    //             //     pageCapacity: 1 // 只取第一个结果
-                    //             // };
-                    //             // const local = new BMap.LocalSearch(map, options);
-                    //             // local.search("医院");
-                    //             // console.log(postion);
-                    //         }
-                    //     })
-                    //
-                    // },error => {
-                    //     console.log(error);
-                    // })
+                    navigator.geolocation.getCurrentPosition(position => {
+                        const ggPoint = new BMap.Point(position.coords.longitude, position.coords.latitude);
+                        const convertor = new BMap.Convertor();
+                        convertor.translate([ggPoint], 1, 5, data => {
+                            if (data.status === 0) {
+                                const bdPoint = data.points[0]
 
-                    geolocation.getCurrentPosition(function (r) {
-                        if (this.getStatus() === BMAP_STATUS_SUCCESS) {
+                                var myGeo = new BMap.Geocoder();
+                                myGeo.getLocation(bdPoint, function(result){
+                                    if (result){
+                                        alert(result.address);
+                                    }
+                                });
 
-                            const mPoint = new BMap.Point(r.point.lng, r.point.lat);
+                                // const map = new BMap.Map(document.createElement('div'));
+                                //
+                                // map.centerAndZoom(bdPoint, 19);
+                                // const options = {
+                                //     onSearchComplete: function (results) {
+                                //         if (local.getStatus() === BMAP_STATUS_SUCCESS) {
+                                //             return resolve(results.getPoi(0).title)
+                                //         }
+                                //     },
+                                //     pageCapacity: 1 // 只取第一个结果
+                                // };
+                                // const local = new BMap.LocalSearch(map, options);
+                                // local.search("医院");
+                                // console.log(postion);
+                            }
+                        })
 
-                            var myGeo = new BMap.Geocoder();
-                            myGeo.getLocation(mPoint, function(result){
-                                if (result){
-                                    alert(result.address);
-                                }
-                            });
-                            // map.centerAndZoom(mPoint, 19);
-                            // const options = {
-                            //     onSearchComplete: function (results) {
-                            //         if (local.getStatus() === BMAP_STATUS_SUCCESS) {
-                            //             for (var i = 0; i < results.getCurrentNumPois(); i++) {
-                            //                 console.log(results.getPoi(i));
-                            //                 // return resolve(results.getPoi(i).title)
-                            //             }
-                            //         }
-                            //     },
-                            //     pageCapacity: 10
-                            // };
-                            // const local = new BMap.LocalSearch(map, options);
-                            // local.searchNearby('医院', mPoint, 1000);
-                        } else {
-                            reject('failed' + this.getStatus())
-                        }
-                    });
+                    },error => {
+                        console.log(error);
+                    })
+
+                    // geolocation.getCurrentPosition(function (r) {
+                    //     if (this.getStatus() === BMAP_STATUS_SUCCESS) {
+                    //
+                    //         const mPoint = new BMap.Point(r.point.lng, r.point.lat);
+                    //
+                    //         var myGeo = new BMap.Geocoder();
+                    //         myGeo.getLocation(mPoint, function(result){
+                    //             if (result){
+                    //                 alert(result.address);
+                    //             }
+                    //         });
+                    //         // map.centerAndZoom(mPoint, 19);
+                    //         // const options = {
+                    //         //     onSearchComplete: function (results) {
+                    //         //         if (local.getStatus() === BMAP_STATUS_SUCCESS) {
+                    //         //             for (var i = 0; i < results.getCurrentNumPois(); i++) {
+                    //         //                 console.log(results.getPoi(i));
+                    //         //                 // return resolve(results.getPoi(i).title)
+                    //         //             }
+                    //         //         }
+                    //         //     },
+                    //         //     pageCapacity: 10
+                    //         // };
+                    //         // const local = new BMap.LocalSearch(map, options);
+                    //         // local.searchNearby('医院', mPoint, 1000);
+                    //     } else {
+                    //         reject('failed' + this.getStatus())
+                    //     }
+                    // });
                 })
             }
         }
