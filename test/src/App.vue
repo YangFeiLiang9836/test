2a227415ae171c6831c5158d5fdf8d0a8591a2cd
363881055@qq.com <yfl@163.com>
2019-05-25 16:07:11 +0800

    1

--- test/src/App.vue
+++ test/src/App.vue
@@ -19,10 +19,6 @@
         methods: {
             getLocation() {
                 return new Promise((resolve, reject) => {
-                    const geolocation = new BMap.Geolocation();
-
-                    const map = new BMap.Map(document.createElement('div'));
-
                     navigator.geolocation.getCurrentPosition(position => {
                         const ggPoint = new BMap.Point(position.coords.longitude, position.coords.latitude);
                         const convertor = new BMap.Convertor();
@@ -30,63 +26,25 @@
                             if (data.status === 0) {
                                 const bdPoint = data.points[0]
 
-                                var myGeo = new BMap.Geocoder();
-                                myGeo.getLocation(bdPoint, function(result){
-                                    if (result){
-                                        alert(result.address);
-                                    }
-                                });
+                                const map = new BMap.Map(document.createElement('div'));
 
-                                // const map = new BMap.Map(document.createElement('div'));
-                                //
-                                // map.centerAndZoom(bdPoint, 19);
-                                // const options = {
-                                //     onSearchComplete: function (results) {
-                                //         if (local.getStatus() === BMAP_STATUS_SUCCESS) {
-                                //             return resolve(results.getPoi(0).title)
-                                //         }
-                                //     },
-                                //     pageCapacity: 1 // 只取第一个结果
-                                // };
-                                // const local = new BMap.LocalSearch(map, options);
-                                // local.search("医院");
-                                // console.log(postion);
+                                map.centerAndZoom(bdPoint, 19);
+                                const options = {
+                                    onSearchComplete: function (results) {
+                                        if (local.getStatus() === BMAP_STATUS_SUCCESS) {
+                                            return resolve(results.getPoi(0).title)
+                                        }
+                                    },
+                                    pageCapacity: 1 // 只取第一个结果
+                                };
+                                const local = new BMap.LocalSearch(map, options);
+                                local.search("医院");
                             }
                         })
 
-                    },error => {
-                        console.log(error);
+                    }, error => {
+                        reject()
                     })
-
-                    // geolocation.getCurrentPosition(function (r) {
-                    //     if (this.getStatus() === BMAP_STATUS_SUCCESS) {
-                    //
-                    //         const mPoint = new BMap.Point(r.point.lng, r.point.lat);
-                    //
-                    //         var myGeo = new BMap.Geocoder();
-                    //         myGeo.getLocation(mPoint, function(result){
-                    //             if (result){
-                    //                 alert(result.address);
-                    //             }
-                    //         });
-                    //         // map.centerAndZoom(mPoint, 19);
-                    //         // const options = {
-                    //         //     onSearchComplete: function (results) {
-                    //         //         if (local.getStatus() === BMAP_STATUS_SUCCESS) {
-                    //         //             for (var i = 0; i < results.getCurrentNumPois(); i++) {
-                    //         //                 console.log(results.getPoi(i));
-                    //         //                 // return resolve(results.getPoi(i).title)
-                    //         //             }
-                    //         //         }
-                    //         //     },
-                    //         //     pageCapacity: 10
-                    //         // };
-                    //         // const local = new BMap.LocalSearch(map, options);
-                    //         // local.searchNearby('医院', mPoint, 1000);
-                    //     } else {
-                    //         reject('failed' + this.getStatus())
-                    //     }
-                    // });
                 })
             }
         }
